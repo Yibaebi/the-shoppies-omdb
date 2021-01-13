62943f4b5a0da9d45ea36375b790ca4a64ec2bcf
Yibaebi <elliot.yibaebi@yahoo.com>
2021-01-13 21:52:21 +0100

    Created a tabs functionality using react-tabs to navigate between MovieList and NominationList

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,68 @@
 import React from "react";
+import { Tabs, TabList, TabPanel, Tab } from "react-tabs";
 import "./App.css";
 import MovieList from "./Movie-List/MovieList";
 import SearchBar from "./Search-Bar/SearchBar";
 import NominationList from "./Nomination-List/NominationList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The Shoppies</h1>
-        <SearchBar />
-      </header>
-      <main className="main-container">
-        <section className="container movie-list">
-          <MovieList />
-        </section>
-        <section className="container nomination-list">
-          <NominationList />
-        </section>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+      active: "active",
+    };
+    this.tabIndex = React.createRef();
+  }
+
+  handleSelect = (index) => {
+    this.setState({ selectedIndex: index });
+  };
+
+  handleButtonClick = () => {
+    this.setState({ selectedIndex: 0 });
+  };
+
+  handleClick = () => {};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>The Shoppies</h1>
+          <SearchBar />
+        </header>
+
+        <Tabs
+          selectedIndex={this.state.selectedIndex}
+          onSelect={this.handleSelect}
+        >
+          <TabList className="tabs-container">
+            <Tab
+              className={`tabs-item ${this.state.active}`}
+              onClick={this.handleClick}
+            >
+              Movies
+            </Tab>
+            <Tab
+              className={`tabs-item ${this.state.active}`}
+              onClick={this.handleClick}
+              tabIndex="2"
+            >
+              Nominations
+            </Tab>
+          </TabList>
+          <main className="main-container">
+            <TabPanel>
+              <MovieList />
+            </TabPanel>
+            <TabPanel>
+              <NominationList />
+            </TabPanel>
+          </main>
+        </Tabs>
+      </div>
+    );
+  }
 }
 
 export default App;
