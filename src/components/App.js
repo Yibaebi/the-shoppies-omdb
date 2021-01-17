f0bb35eb8cb9579133cc952b9e6f86ff2faeaede
Yibaebi <elliot.yibaebi@yahoo.com>
2021-01-17 16:25:39 +0100

    Updated hero style

--- src/components/App.js
+++ src/components/App.js
@@ -249,9 +249,8 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <div className="hero">
-            <i className="fab fa-shopify"></i>
             <h1>
-              The <span>Shopp</span>ies
+              <i className="fab fa-shopify"></i>The <span>Shopp</span>ies
             </h1>
             <p>"Movie awards for entrepreneurs"</p>
             <div className="ripple-background">
