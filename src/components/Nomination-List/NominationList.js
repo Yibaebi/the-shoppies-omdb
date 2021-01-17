6107b79d2e8e722038a7e5cfaaf9a1a564b5f02c
Yibaebi <elliot.yibaebi@yahoo.com>
2021-01-17 13:04:44 +0100

    Refactored NominationList component added a new loader for movie list.

--- src/components/Nomination-List/NominationList.js
+++ src/components/Nomination-List/NominationList.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import NominationItem from "./NominationItem";
 
-class NominationList extends Component {
-  render() {
-    const NominationList = this.props.Nominations.map((Nomination) => (
-      <div className="nomination-item" key={Nomination.imdbID}>
-        <NominationItem
-          Nominated={Nomination}
-          removeNomination={this.props.removeNomination}
-        />
-      </div>
-    ));
-    return <div className={`nomination-container`}>{NominationList}</div>;
-  }
-}
+const NominationList = (props) => {
+  const NominationList = props.Nominations.map((Nomination) => (
+    <div className="nomination-item" key={Nomination.imdbID}>
+      <NominationItem
+        Nominated={Nomination}
+        removeNomination={props.removeNomination}
+      />
+    </div>
+  ));
+  return <div className={`nomination-container`}>{NominationList}</div>;
+};
 
 export default NominationList;
--- src/utilities/loaders/loaders.js
+++ src/utilities/loaders/loaders.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { ClipLoader } from "react-spinners";
+import { ClipLoader, ScaleLoader } from "react-spinners";
 
-export default function loaders() {
+export default function primaryLoader() {
   return <ClipLoader loading size={16} color="#fff" />;
+}
+
+export function MovieLoader() {
+  return (
+    <ScaleLoader color="#3f3d56" height={35} width={3} radius={4} margin={2} />
+  );
 }
