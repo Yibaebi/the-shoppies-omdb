6a7a39c3e3035c03cd54d0ca026b632d6d4e0348
Yibaebi <elliot.yibaebi@yahoo.com>
2021-01-17 11:11:10 +0100

    Refactored methods in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,56 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    this._retrieveNominationsFromStorage();
+
+    this.handleSearchSubmit = async (e) => {
+      e.preventDefault();
+
+      this.setState({
+        searchIcon: <Loader />,
+        selectedIndex: 0,
+      });
+
+      const MoviesList = await this.getMoviesFromOMDB();
+
+      //Remove loader in search bar if response is true.
+      if (MoviesList.length) {
+        this.setState({
+          searchIcon: <i className="fa fa-search"></i>,
+        });
+      }
+
+      //Give each result a nomination status of false
+      MoviesList.forEach(function (movie) {
+        movie.nominated = false;
+        movie.label = "Nominate";
+      });
+
+      titlesAreSame(MoviesList, this.state.Nominations);
+      this._disableNominationButtons(MoviesList);
+
+      this.setState({
+        Movies: MoviesList,
+      });
+    };
+  }
+
+  getMoviesFromOMDB = async () => {
+    const response = await fetch(
+      `https://www.omdbapi.com/?s=${this.state.searchQuery}&apikey=d2850ca8`
+    );
+    const responseJSON = await response.json();
+    const MoviesList = responseJSON.Search;
+    return MoviesList;
+  };
+
+  //Function to scitch tabs
+  handleSelect = (index) => {
+    this.setState({ selectedIndex: index });
+  };
+
+  //Function to retrieve nominations
+  _retrieveNominationsFromStorage = () => {
     const storedNominations = JSON.parse(
       localStorage.getItem("shopify-movie-app")
     );
@@ -32,81 +82,14 @@ class App extends React.Component {
         active1: "tabs-item active",
       });
     }
-
-    this.handleSearchSubmit = async (e) => {
-      e.preventDefault();
-
-      this.setState({
-        searchIcon: <Loader />,
-        selectedIndex: 0,
-      });
-
-      const response = await fetch(
-        `https://www.omdbapi.com/?s=${this.state.searchQuery}&apikey=d2850ca8`
-      );
-      const responseJSON = await response.json();
-      const MoviesList = responseJSON.Search;
-
-      if (MoviesList.length) {
-        this.setState({
-          searchIcon: <i className="fa fa-search"></i>,
-        });
-      }
-
-      MoviesList.forEach(function (movie) {
-        movie.nominated = false;
-        movie.label = "Nominate";
-      });
-
-      titlesAreSame(MoviesList, this.state.Nominations);
-
-      if (this.state.Nominations.length >= 5) {
-        MoviesList.map((movie) => (movie.nominated = true));
-      }
-
-      this.setState({
-        Movies: MoviesList,
-      });
-    };
-  }
-
-  handleSelect = (index) => {
-    this.setState({ selectedIndex: index });
   };
 
-  saveToLocalStorage = (list) => {
+  //Function to save nominations
+  _saveToLocalStorage = (list) => {
     localStorage.setItem("shopify-movie-app", JSON.stringify(list));
   };
 
-  handleNomination = (e, movie) => {
-    const Movies = this.state.Movies;
-    Movies.map((movieItem) => {
-      if (movie.imdbID === movieItem.imdbID) {
-        movieItem.nominated = true;
-        movieItem.label = "Nominated";
-      }
-      return movieItem;
-    });
-
-    const NominationList = [...this.state.Nominations, movie];
-    if (NominationList.length >= 5) {
-      const newMovies = this.state.Movies.map(
-        (movie) => (movie.nominated = true)
-      );
-
-      this.setState({
-        Movies: newMovies,
-      });
-    }
-
-    this.setState({
-      Nominations: NominationList,
-      Movies: Movies,
-    });
-
-    this.saveToLocalStorage(NominationList);
-  };
-
+  //Function to switch between main tabs (Movie and Nomination)
   handleTabActiveness = (e) => {
     if (e.target.tabIndex === 1) {
       this.setState({
@@ -121,14 +104,50 @@ class App extends React.Component {
     }
   };
 
+  //Function to add nominations to Nomination list
+  handleNomination = (e, movie) => {
+    const Movies = this.state.Movies;
+    Movies.map((movieItem) => {
+      if (movie.imdbID === movieItem.imdbID) {
+        movieItem.nominated = true;
+        movieItem.label = "Nominated";
+      }
+      return movieItem;
+    });
+
+    //Disable nomination buttons after five nominations
+    const NominationList = [...this.state.Nominations, movie];
+    if (NominationList.length >= 5) {
+      const newMovies = this.state.Movies;
+      newMovies.map((movie) => {
+        movie.nominated = true;
+        if (movie.label === "Nominate") {
+          movie.label = "Can't Nominate";
+        }
+        return movie;
+      });
+
+      this.setState({
+        Movies: newMovies,
+      });
+    }
+
+    this.setState({
+      Nominations: NominationList,
+      Movies: Movies,
+    });
+
+    this._saveToLocalStorage(NominationList);
+  };
+
   //Function to remove nominations from list
   removeNomination = (e, nominated) => {
     const newNominationList = this.state.Nominations;
+    let updateMoviesList = this.state.Movies;
+
     const newNominations = newNominationList.filter(
       (nomination) => nomination.Title !== nominated.Title
     );
-
-    let updateMoviesList = this.state.Movies;
 
     updateMoviesList.map((movieItem) => {
       if (nominated.imdbID === movieItem.imdbID) {
@@ -142,13 +161,26 @@ class App extends React.Component {
       updateMoviesList,
       newNominations
     );
-    this.saveToLocalStorage(newNominations);
+    this._saveToLocalStorage(newNominations);
 
     this.setState({
       Nominations: newNominations,
       Movies: updateMoviesList,
     });
   };
+
+  //Disable nomination buttons if five nominations already exists in list
+  _disableNominationButtons(MoviesList) {
+    if (this.state.Nominations.length >= 5) {
+      MoviesList.map((movie) => {
+        movie.nominated = true;
+        if (movie.label === "Nominate") {
+          movie.label = "Can't Nominate";
+        }
+        return movie;
+      });
+    }
+  }
 
   render() {
     return (
@@ -214,10 +246,12 @@ class App extends React.Component {
   }
 }
 
+//Function to check search results for already exisiting nominations in local storage and disable them.
 const titlesAreSame = (moviesList, nominationList) => {
-  for (let nominated in nominationList) {
+  const Nominations = nominationList;
+  for (let nominated in Nominations) {
     for (let Title in moviesList) {
-      if (moviesList[Title].Title === nominationList[nominated].Title) {
+      if (moviesList[Title].Title === Nominations[nominated].Title) {
         moviesList[Title].nominated = true;
         moviesList[Title].label = "Nominated";
       }
@@ -225,12 +259,16 @@ const titlesAreSame = (moviesList, nominationList) => {
   }
 };
 
+//Function to restore nomination status of unselected movies
 const restoreNominationStatus = (moviesList, nominationList) => {
   for (let nomination in nominationList) {
     for (let Title in moviesList) {
       if (moviesList[Title].label === "Nominated") {
         continue;
-      } else if (moviesList[Title].label === "Nominate") {
+      } else if (
+        moviesList[Title].label === "Nominate" ||
+        moviesList[Title].label === "Can't Nominate"
+      ) {
         moviesList[Title].nominated = false;
         moviesList[Title].label = "Nominate";
       }
